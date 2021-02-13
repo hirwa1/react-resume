84af414b9370be1910a5cfa09e373dfc573b8dff
hirwa1 <hirwadeveloper@gmail.com>
2021-02-13 16:26:17 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+const hirwa = <h1>My name is hirwa</h1>;
+ReactDOM.render(hirwa,  document.getElementById("root"));
