2474722c7c7c7c9a40ea9f938f25367a586468c2
hirwa1 <hirwadeveloper@gmail.com>
2021-02-21 13:24:52 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-const hirwa = <h1>My name is hirwa</h1>;
+const hirwa = <h1>Try to be the first</h1>;
 const off = <div><h3>Be on The top </h3> {hirwa }</div>;
 ReactDOM.render(off,  document.getElementById("root"));
