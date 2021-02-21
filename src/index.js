3fd16bbe2668e4dc6ad45582741d061127887e06
hirwa1 <hirwadeveloper@gmail.com>
2021-02-21 13:27:04 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ import App from "./App";
 const hirwa = <h1>Try to be the first</h1>;
 const off = <div><h3>Be on The top </h3> {hirwa }</div>;
 ReactDOM.render(off,  document.getElementById("root"));
+ReactDOM.render(hirwa , document.getElementById("ok"))
