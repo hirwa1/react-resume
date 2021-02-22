0990bd28f133269d1191338272c0d31461891a6e
hirwa1 <hirwadeveloper@gmail.com>
2021-02-22 13:47:59 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from "./App";
 const hirwa = <h1>Try to be the first</h1>;
 const time = <h1>This is my Time Initialization</h1>;
 const off = <div><h3>Be on The top </h3> {hirwa }</div>;
-ReactDOM.render(off,  document.getElementById("root"));
+ReactDOM.render(< App/>,  document.getElementById("root"));
 ReactDOM.render(time , document.getElementById("ok"))
