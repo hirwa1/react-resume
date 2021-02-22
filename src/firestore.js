3f5626d4c3d831eb8dfc10cf761199ea0b8475cc
hirwa1 <hirwadeveloper@gmail.com>
2021-02-22 13:48:58 +0200

    1 file modified

---
+++ src/firestore.js
@@ -0,0 +1,9 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAmIag9p5m71dLPZHGzC72qnWA-oscmmyI",
+    authDomain: "next-edu-board.firebaseapp.com",
+    projectId: "next-edu-board",
+    storageBucket: "next-edu-board.appspot.com",
+    messagingSenderId: "96720071869",
+    appId: "1:96720071869:web:d9fbfc7a5095cd6e505bd4",
+    measurementId: "G-Q3SM8J2JV1"
+  };
